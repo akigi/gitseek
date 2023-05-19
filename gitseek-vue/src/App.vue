fd74637be17fba52136d4c8407c7de2559dd11bc
akigi <akipon.dev@gmail.com>
2023-05-19 21:58:39 +0900

    wip

--- gitseek-vue/src/App.vue
+++ gitseek-vue/src/App.vue
@@ -1,58 +1,58 @@
 <template>
-  <div id="app">
-    <div class="control_wrapper">
-        <ejs-treeview id='treeview' :fields="fields"></ejs-treeview>
-    </div>
+  <div class="control_wrapper">
+     <ejs-treeview id='treeview' :fields='fields'></ejs-treeview>
   </div>
 </template>
 <script>
 import Vue from 'vue';
 import { TreeViewPlugin } from "@syncfusion/ej2-vue-navigations";
 
+// simple-git
+// import { simpleGit, CleanOptions } from 'simple-git';
+// simpleGit().clean(CleanOptions.FORCE);
+
+// // child_process
+// import child_process from 'child_process';
+// // const { spawn } = child_process;
+// // const ls = spawn('ls', ['-lh', '/usr']);
+// // ls.stdout.on('data', data => console.log(`stdout: ${data}`));
+// // ls.stderr.on('data', data => console.log(`stderr: ${data}`));
+// // ls.on('close', code => code && console.log(`child process exited with code ${code}.`));
+
+
+// Vue
 Vue.use(TreeViewPlugin);
-export default {
-  data () {
-    var dataSource =  [
-       {
-        nodeId: '01', nodeText: 'Music',
-        nodeChild: [
-            { nodeId: '01-01', nodeText: 'Gouttes.mp3' }
-        ]
-    },
-    {
-        nodeId: '02', nodeText: 'Videos', expanded: true,
-        nodeChild: [
-            { nodeId: '02-01', nodeText: 'Naturals.mp4' },
-            { nodeId: '02-02', nodeText: 'Wild.mpeg' },
-        ]
-    },
-    {
-        nodeId: '03', nodeText: 'Documents',
-        nodeChild: [
-            { nodeId: '03-01', nodeText: 'Environment Pollution.docx' },
-            { nodeId: '03-02', nodeText: 'Global Water, Sanitation, & Hygiene.docx' },
-            { nodeId: '03-03', nodeText: 'Global Warming.ppt' },
-            { nodeId: '03-04', nodeText: 'Social Network.pdf' },
-            { nodeId: '03-05', nodeText: 'Youth Empowerment.pdf' },
-        ]
-    }
-    ];
-    return {
-      fields: { dataSource: dataSource, id: 'nodeId', text: 'nodeText', child: 'nodeChild' }
-    }
-  }
+
+var data =  [
+  {
+      nodeId: '01', nodeText: 'Music',
+      nodeChild: [
+          { nodeId: '01-01', nodeText: 'Gouttes.mp3' }
+      ]
+  },
+  {
+      nodeId: '02', nodeText: 'Videos', expanded: true,
+      nodeChild: [
+          { nodeId: '02-01', nodeText: 'Naturals.mp4' },
+          { nodeId: '02-02', nodeText: 'Wild.mpeg' },
+      ]
+  },
+  {
+      nodeId: '03', nodeText: 'Documents',
+      nodeChild: [
+          { nodeId: '03-01', nodeText: 'Environment Pollution.docx' },
+          { nodeId: '03-02', nodeText: 'Global Water, Sanitation, & Hygiene.docx' },
+          { nodeId: '03-03', nodeText: 'Global Warming.ppt' },
+          { nodeId: '03-04', nodeText: 'Social Network.pdf' },
+          { nodeId: '03-05', nodeText: 'Youth Empowerment.pdf' },
+      ]
+  },
+];
+export default Vue.extend({
+data: function() {
+  return {
+  fields: { dataSource: data, id: 'nodeId', text: 'nodeText', child: 'nodeChild' },
+  };
 }
+});
 </script>
-<style>
-  @import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
-  @import "../../node_modules/@syncfusion/ej2-vue-navigations/styles/material.css";
- .control_wrapper {
-        display: block;
-        max-width: 350px;
-        max-height: 350px;
-        margin: auto;
-        overflow: auto;
-        border: 1px solid #dddddd;
-        border-radius: 3px;
-    }
-</style>
--- gitseek-vue/src/main.js
+++ gitseek-vue/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
+import child_process from 'child_process';
+const { spawn } = child_process;
+const ls = spawn('ls', ['-lh', '/usr']);
+ls.stdout.on('data', data => console.log(`stdout: ${data}`));
+ls.stderr.on('data', data => console.log(`stderr: ${data}`));
+ls.on('close', code => code && console.log(`child process exited with code ${code}.`));
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
